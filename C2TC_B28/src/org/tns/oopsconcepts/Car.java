8feba87315433bbf34a52470ea1c71faa4d3e796
Divya <Divya@DESKTOP-NIEE4B9>
2022-10-17 21:11:51 +0530

    commit

--- C2TC_B28/src/org/tns/oopsconcepts/Car.java
+++ C2TC_B28/src/org/tns/oopsconcepts/Car.java
@@ -4,18 +4,6 @@ public class Car {
 	void displaynames() {
 		System.out.println("Following are the models of Car");
 	}
-	public static void main(String[] args) {
-		Bmw b1= new Bmw();
-		b1.displaynames();
-		b1.show1();
-		seden b2= new seden();
-		
-		b2.show2();
-		suvu b3= new suvu();
-		
-		b3.show3();
-		
-	}
 
 }
 class Bmw extends Car{
--- C2TC_B28/src/org/tns/oopsconcepts/Dasharatha.java
+++ C2TC_B28/src/org/tns/oopsconcepts/Dasharatha.java
@@ -16,10 +16,5 @@ public class Dasharatha {
 	void display1() {
 		System.out.println("I am the grandfather");
 	}
-	public static void main(String[] args) {
-		Lava b1= new Lava();
-		b1.display1();
-		b1.display2();
-		b1.display3();
-}
+	
 }
